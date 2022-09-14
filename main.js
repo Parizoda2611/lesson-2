7dcb504babc4f442f369fa73b3040a6a15e66610
Parizoda2611 <parizodarakhmankulova@gmail.com>
2022-09-14 15:43:05 +0500

    Hometask

---
+++ main.js
@@ -0,0 +1,34 @@
+/* var student = {
+  name: "Toxir",
+  age: 18,
+  score: 77,
+  address: "Kokand",
+  married: false,
+};
+
+
+if (student.score > 70) {
+  console.log("Siz talabalikka qabul qilindingiz !");
+}
+
+if (student.married == true) {
+  console.log("Sizga qo'shimcha pul mablag'i ajratildi");
+} else{
+  console.log("Ko'proq o'qing !");
+} */
+
+/* function logName(x, y) {
+  console.log(x + y);
+}
+
+logName(3, 5)
+
+logName (4, 5) */
+
+function sevimliKitobQoshish(kitobNomi) {
+  if(kitobNomi.includes("Great")) {
+    sevimliKitobjavoni.push(kitobNomi)
+  }
+}
+ var sevimliKitobjavoni = []
+
---
+++ practice.js
@@ -0,0 +1,83 @@
+/* const number = (prompt("Enter a number: "));
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("The number is negative");
+} */
+
+/* const number = prompt("Enter a number: ");
+
+if(number % 2 == 0) {
+  console.log("The number is even.");
+}
+
+else {
+  console.log("The number is odd.");
+} */
+
+/* let a = prompt("Enter a number: ")
+let b = prompt("Enter a number: ")
+
+if(a > 0 && b > 0){
+  console.log("True")
+}
+
+else{
+  console.log("False")
+} */
+
+/* let a = 4
+let b = 6
+let c = 8
+
+if(a < b && b > c){
+  console.log("True");
+}
+else{
+  console.log("False");
+} */
+
+/* const a = prompt("Enter a number: ");
+const b = prompt("Enter a number: ");
+
+if(number % 2 == 0) {
+  console.log("The number is even.");
+}
+
+else {
+  console.log("The number is odd.");
+}  */
+
+/* let a = 1
+let b = 1
+let c = 1
+let d = 1
+ 
+if(a > 0 && b > 0 && c > 0 && d > 0){
+  console.log("True");
+}
+
+else{
+  console.log("False");
+} */
+
+
+/* let a = prompt("Enter a number: ")
+let b = prompt("Enter a number: ")
+let c = prompt("Enter a number: ")
+
+if(a > 0 && b > 0 && c < 0){
+  console.log("True");
+}
+else{
+  console.log("False");
+} */
+
